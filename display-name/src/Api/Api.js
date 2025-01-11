84502acc86c3b57222db7c5a707d2d1043daf617
lkonsam <lkonsam@gmail.com>
2025-01-11 13:34:56 +0530

    display-name

---
+++ display-name/src/Api/Api.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+const API_END_POINT = `https://crio-location-selector.onrender.com`;
+
+export async function fetchCountries(){
+    try {
+        const response = await axios.get(`${API_END_POINT}/countries`);
+        return response.data;
+
+    } catch (error) {
+        console.error("Error fetching data: ",error);
+        return [];
+    }
+}
+export async function fetchStates(countryName){
+    try {
+        const response = await axios.get(`${API_END_POINT}/country=${countryName}/states`);
+        return response.data;
+
+    } catch (error) {
+        console.error("Error fetching data: ",error);
+        return [];
+    }
+}
+export async function fetchCities(countryName, stateName){
+    try {
+        const response = await axios.get(`${API_END_POINT}/country=${countryName}/state=${stateName}/cities`);
+        return response.data;
+
+    } catch (error) {
+        console.error("Error fetching data: ",error);
+        return [];
+    }
+}
+
