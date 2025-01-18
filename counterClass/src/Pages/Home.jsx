15fed6862062e7189b0dfa7bc48ce3e292cf3f2e
lkonsam <lkonsam@gmail.com>
2025-01-18 14:55:52 +0530

    counterClass added

---
+++ counterClass/src/Pages/Home.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+
+export default class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+
+  increment = () => {
+    this.setState((prevState) => ({
+      count: prevState.count + 1,
+    }));
+  };
+
+  decrement = () => {
+    this.setState((prevState) => ({
+      count: prevState.count - 1,
+    }));
+  };
+
+  render() {
+    return (
+      <>
+        <h1>Counter App</h1>
+        <p>Count: {this.state.count}</p>
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.decrement}>Decrement</button>
+      </>
+    );
+  }
+}
